32ca115ccd44f4cc05c42691f8d369c28311cd4f
KRUROL <rolands.krumbergs@if.lv>
2017-11-01 13:56:34 +0200

    Removed everything

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 $(document).ready(function() { 
 
-    var layoutSettings = {
-        "headerHeight": "20%",
-        "bodyHeight": "75%",
-        "footerHeight": "5%",
-    };
-
     $("#body_container").css("display", "block");
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
@@ -37,69 +31,4 @@ $(document).ready(function() {
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
 
-    $("#navigation_buttons p").hover(function(){
-        $(this).css("color", "black");
-    }, function() {
-        $(this).css("color", "white");
-    });
-
-    $("#navigation_buttons p").click(function(){
-        var text = $(this).text();
-        if(text === "Par mani") {
-            $("#body_container").css("display", "block");
-            $("#image_container").css("display", "none");
-            $("#form_content").css("display", "none");
-        }
-
-        if(text === "Darbi") {
-            $("#body_container").css("display", "none");
-            $("#image_container").css("display", "block");
-            $("#form_content").css("display", "none");
-        }
-
-        if(text === "Sazinaties") {
-            $("#body_container").css("display", "none");
-            $("#image_container").css("display", "none");
-            $("#form_content").css("display", "block");
-        }
-    });
-
-    for (var index = 0; index < 3; index++) {
-        $.ajax({
-            type: 'GET',
-            url: 'http://thecatapi.com/api/images/get?format=html',
-            dataType: 'text',
-            success: function(data) { 
-                var output = data;
-                
-                var div = $("<div></div>");
-                div.addClass("col-sm-4");
-    
-                var image = $(output);
-                image.find("img").css("height", "350px")
-                image.find("img").css("width", "350px")
-                div.append(image);
-    
-                $("#image_content").append(div);
-            },
-            error: function(data) { alert('fail') }
-          });
-    }
-
-    //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
-
-    var email = {"personalizations": [{"to": [{"email": "rolands.krumbergs@outlook.com"}]}],"from": {"email": "example@example.com"},"subject": "Hello, World!","content": [{"type": "text/plain", "value": "Heya!"}]}
-
-    $.ajax({
-        type: 'POST',
-        url: 'https://api.sendgrid.com/v3/mail/send',
-        dataType: 'json',
-        data: email,
-        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', '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');},
-        success: function(data) { 
-            var output = data;
-            alert(output);
-        },
-        error: function(data) { alert('fail') }
-      });
 });
