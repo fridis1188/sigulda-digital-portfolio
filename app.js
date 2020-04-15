a8d45455a2765099904bc041abd17f8c4bc46ba7
fridis1188 <58549488+fridis1188@users.noreply.github.com>
2020-04-15 15:44:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 $(document).ready(function() { 
 
-    $("#body_container").css("display", "block");
-    $("#image_container").css("display", "none");
-    $("#form_content").css("display", "none");
+    $("#home_container").css("display", "block");
+    $("#about_me_container").css("display", "none");
+    $("#my_work_container").css("display", "none");
+    $("#contact_container").css("display","none")
 
-    $("#header").css("background", "black");
+    $("#header").css("background", "#45A29E");
     $("#header").css("width", "100%");
     $("#header").css("height", "20%");
 
-    $("#body").css("background", "blue");
-    $("#body").css("width", "100%");
+    $("#body").css("background", "#1F2833");
+    $("#body").css("width", "90%");
     $("#body").css("height", "75%");
-    
-    $("#footer").css("background", "red");
-    $("#footer").css("width", "100%");
-    $("#footer").css("height", "5%");
 
-    $("#navigation_header").css("padding-top", "20px");
-    $("#navigation_header").css("padding-bottom", "20px");
+    $("#button_row").css("background", "black");
+    $("#button_row").css("width", "100%");
+    $("#button_row").css("height", "5%");
+
+    $("#title").css("padding-top", "20px");
+    $("#title").css("padding-bottom", "20px");
+    $("#title").css("color", "white");
+
+    $("#navigation_buttons").css("color", "white");
 
     $("#body_content").css("background", "yellow");
 
@@ -30,5 +34,41 @@ $(document).ready(function() {
 
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
+
+    $("#bar_right").css("height", "75%");
+    $("#bar_right").css("width", "5%");
+    $("#bar_right").css("background", "#45A29E");
+
+    $("#bar_left").css("height", "75%");
+    $("#bar_left").css("width", "5%");
+    $("#bar_left").css("background", "#45A29E");
+
+    $("#home").click(function() {
+        $("#home_container").css("display", "block");
+        $("#about_me_container").css("display", "none");
+        $("#my_work_container").css("display", "none");
+        $("#contact_container").css("display","none")
+    });
+
+    $("#about_me").click(function() {
+        $("#home_container").css("display", "none");
+        $("#about_me_container").css("display", "block");
+        $("#my_work_container").css("display", "none");
+        $("#contact_container").css("display","none")
+    });
+
+    $("#my_work").click(function() {
+        $("#home_container").css("display", "none");
+        $("#about_me_container").css("display", "none");
+        $("#my_work_container").css("display", "block");
+        $("#contact_container").css("display","none")
+    });
+
+    $("#contact").click(function() {
+        $("#home_container").css("display", "none");
+        $("#about_me_container").css("display", "none");
+        $("#my_work_container").css("display", "none");
+        $("#contact_container").css("display","block")
+    });
 
 });
